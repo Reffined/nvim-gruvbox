c7b6c9100a9211e9a155802deef72589605bb463
havok <alireza.karampoor@gmail.com>
2024-10-30 20:42:32 +0330

    from home

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Default keymaps that are always set: https//github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "<C-L>", "<Right>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-H>", "<Left>", { noremap = true, silent = true })
